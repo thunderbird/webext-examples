43abb0a97615969520d4f84984941d559af41aef
Geoff Lankow <geoff@darktrojan.net>
2018-09-22 00:17:02 +1200

    Use mozilla's linting rules, plus some of my own

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "plugin:mozilla/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 9,
+  },
+  globals: {
+    browser: true,
+  },
+  rules: {
+    "curly": 2,
+    "indent-legacy": [2, 2, { SwitchCase: 1, }],
+    "object-curly-newline": [2, { multiline: true }],
+    "padded-blocks": [2, "never"],
+  },
+};
--- addressBooks/background.js
+++ addressBooks/background.js
@@ -1,11 +1,7 @@
 if ("browserAction" in browser) {
-	browser.browserAction.onClicked.addListener(async () => {
-		browser.tabs.create({
-			url: browser.extension.getURL("page.html"),
-		});
-	});	
+  browser.browserAction.onClicked.addListener(async () => {
+    browser.tabs.create({ url: browser.extension.getURL("page.html") });
+  });
 } else {
-	browser.tabs.create({
-		url: browser.extension.getURL("page.html"),
-	});
+  browser.tabs.create({ url: browser.extension.getURL("page.html") });
 }
