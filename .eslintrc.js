474021cdc7f686b15d0ae81e86f533d46cf95c7f
Geoff Lankow <geoff@darktrojan.net>
2019-10-10 15:07:58 +1300

    Turn off prettier and restore rules removed when it was enabled

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     "plugin:mozilla/recommended",
   ],
+  plugins: ["mozilla"],
   parserOptions: {
     ecmaVersion: 9,
   },
@@ -20,5 +21,50 @@ module.exports = {
     "indent": [2, 2, { SwitchCase: 1, }],
     "object-curly-newline": [2, { multiline: true }],
     "padded-blocks": [2, "never"],
+    "prettier/prettier": 0,
+
+    "arrow-spacing": "error",
+    "block-spacing": "error",
+    "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+    "comma-dangle": ["error", "always-multiline"],
+    "comma-spacing": ["error", {"after": true, "before": false}],
+    "comma-style": "error",
+    "computed-property-spacing": ["error", "never"],
+    "curly": "off",
+    "eol-last": "error",
+    "func-call-spacing": "error",
+    "generator-star-spacing": ["error", {"after": true, "before": false}],
+    "key-spacing": ["error", {
+      "afterColon": true,
+      "beforeColon": false,
+      "mode": "minimum",
+    }],
+    "keyword-spacing": "error",
+    "linebreak-style": ["error", "unix"],
+    "no-tabs": "error",
+    "no-trailing-spaces": "error",
+    "no-whitespace-before-property": "error",
+    "padded-blocks": ["error", "never"],
+    "quotes": ["error", "double", {
+      "allowTemplateLiterals": true,
+      "avoidEscape": true,
+    }],
+    "rest-spread-spacing": "error",
+    "semi": ["error", "always"],
+    "space-before-blocks": "error",
+    "space-before-function-paren": ["error", {
+      "anonymous": "never",
+      "asyncArrow": "always",
+      "named": "never",
+    }],
+    "space-infix-ops": ["error", { "int32Hint": true }],
+    "space-unary-ops": ["error", {
+      "nonwords": false,
+      "overrides": {
+        "typeof": false, // We tend to use typeof as a function call
+      },
+      "words": true,
+    }],
+    "spaced-comment": ["error", "always", { "markers": ["#"] }],
   },
 };
