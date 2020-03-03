09219b5d3e507aa74ca2907c879acc1807f8caef
Geoff Lankow <geoff@darktrojan.net>
2020-03-03 21:41:25 +1300

    Add module unloading and some corrections

--- experiment/implementation.js
+++ experiment/implementation.js
@@ -10,15 +10,15 @@ var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 // in legacy add-ons. However, chrome:// urls that point to add-on resources no
 // longer work, as the "chrome.manifest" file is no longer supported, which
 // defined the root path for each add-on. Instead, ChromeUtils.import() needs
-// a mozExtension:// url, which can access any resource in an add-on:
+// a moz-extension:// url, which can access any resource in an add-on:
 //
-// mozExtension://<Add-On-UUID>/path/to/modue.jsm
+// moz-extension://<Add-On-UUID>/path/to/modue.jsm
 //
 // The add-on UUID is a random identifier generated on install for each add-on.
 // The extension object of the WebExtension has a getURL() method, to get the
 // required url:
 //
-// let mozExtensionUrl = extension.getURL("path/to/modue.jsm");
+// let mozExtensionUrl = extension.getURL("path/to/module.jsm");
 //
 // You can get the extension object from the context parameter passed to
 // getAPI() of the WebExtension experiment implementation:
@@ -26,17 +26,21 @@ var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 // let extension = context.extension;
 //
 // or you can generate the extension object from a given add-on ID as shown in
-// the example below. This allows to import JSM out of context, for example
-// inside another JSM. 
+// the example below. This allows you to import JSM without context, for example
+// inside another JSM.
 //
 var { ExtensionParent } = ChromeUtils.import("resource://gre/modules/ExtensionParent.jsm");
-let extension = ExtensionParent.GlobalManager.getExtension("experiment@sample.extensions.thunderbird.net");
+var extension = ExtensionParent.GlobalManager.getExtension("experiment@sample.extensions.thunderbird.net");
 var { myModule } = ChromeUtils.import(extension.getURL("modules/myModule.jsm"));
 
 // This is the important part. It implements the functions and events defined in schema.json.
 // The variable must have the same name you've been using so far, "myapi" in this case.
 var myapi = class extends ExtensionCommon.ExtensionAPI {
   getAPI(context) {
+    // To be notified of the extension going away, call callOnClose with any object that has a
+    // close function, such as this one.
+    context.callOnClose(this);
+
     return {
       // Again, this key must have the same name.
       myapi: {
@@ -68,6 +72,18 @@ var myapi = class extends ExtensionCommon.ExtensionAPI {
 
       },
     };
+  }
+
+  close() {
+    // This function is called if the extension is disabled or removed, or Thunderbird closes.
+    // We registered it with callOnClose, above.
+    console.log("Goodbye world!");
+
+    // Unload the JSM we imported above. This will cause Thunderbird to forget about the JSM, and
+    // load it afresh next time `import` is called. (If you don't call `unload`, Thunderbird will
+    // remember this version of the module and continue to use it, even if your extension receives
+    // an update.) You should *always* unload JSMs provided by your extension.
+    Cu.unload(extension.getURL("modules/myModule.jsm"));
   }
 };
 
