ce2e9adbf88a2f6ae296b17a2441b1e49a25bc5d
John Bieling <john.bieling@gmx.de>
2020-11-12 12:51:45 +0100

    Load api.js in all other already intended windows

--- apiList/background.js
+++ apiList/background.js
@@ -5,3 +5,16 @@ if ("browserAction" in browser) {
 } else {
   browser.tabs.create({ url: "apis.html" });
 }
+
+async function injectIntoContent(tab, info) {
+  await browser.tabs.insertCSS(tab.id, {
+    file: "apis.css"
+  });
+  await browser.tabs.executeScript(tab.id, {
+    file: "apis.js"
+  })
+}
+
+browser.composeAction.onClicked.addListener(injectIntoContent);
+browser.messageDisplayAction.onClicked.addListener(injectIntoContent);
+
