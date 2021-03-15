def25ea24dd5fb7206587ecad01ebf558dc591b8
John Bieling <john.bieling@gmx.de>
2021-03-15 23:17:31 +0100

    add support for pdf preview

--- attachmentPreview/popup.js
+++ attachmentPreview/popup.js
@@ -1,3 +1,15 @@
+async function pdfPreview(url, canvas) {
+//  pdfjsLib.disableWorker = true;
+  let pdf = await pdfjsLib.getDocument(url).promise;  
+  let page = await pdf.getPage(1);
+  let  viewport = await page.getViewport({scale: 1.0});
+  let  scale = Math.min(canvas.width / viewport.width, canvas.height / viewport.height);
+  let  thumbViewPort = await page.getViewport({scale});  
+  let context = canvas.getContext('2d');
+  page.render({canvasContext: context, viewport: thumbViewPort});
+}
+
+
 async function generateThumbs() {
   let url = new URL(window.location.href);
   let tabId = parseInt(url.searchParams.get("tabId"), 10);
@@ -25,11 +37,6 @@ async function generateThumbs() {
     let id = `attachmentElement_${messages[0].id}_${attachment.partName}`;
     let t = document.querySelector('#attachmentTemplate');
     t.content.querySelector("div").id = id;
-    if (attachment.contentType.toLowerCase().startsWith("image/"))
-      t.content.querySelector("img").src = attachment.url;
-    else
-      t.content.querySelector("img").display = "none";
-    
     t.content.querySelector("p").textContent = attachment.name;
     document.body.appendChild(document.importNode(t.content, true));
     
@@ -41,6 +48,17 @@ async function generateThumbs() {
       button.setAttribute("data-attachment-part-name", attachment.partName);
       button.addEventListener("click", clickHandler);
     }
+
+    if (attachment.contentType.toLowerCase().startsWith("image/"))
+      attachmentElement.querySelector("img").src = attachment.url;
+    else
+      attachmentElement.querySelector("img").style.display = "none";
+
+    if (attachment.contentType.toLowerCase() == "application/pdf")
+      pdfPreview(attachment.url, attachmentElement.querySelector("canvas"));    
+    else
+      attachmentElement.querySelector("canvas").style.display = "none";
+    
   }  
 }
 
