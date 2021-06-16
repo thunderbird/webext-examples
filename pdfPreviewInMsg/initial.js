d3c8da102a44cbec2c427184ab893e0bc763d0a3
John Bieling <john.bieling@gmx.de>
2021-06-16 12:56:44 +0200

    eslint pass + var -> let + comments updates

--- pdfPreviewInMsg/initial.js
+++ pdfPreviewInMsg/initial.js
@@ -5,67 +5,71 @@ This extension for Thunderbird was made by : e-gaulue & rholeczy
 ----------------------------------------------------------------
 */
 
-
 var divCree = false;
 
-browser.runtime.onMessage.addListener((data, sender) => { // RH : Listener if we received a message.
+// RH : Listener if we received a message.
+browser.runtime.onMessage.addListener((data, sender) => {
+  if (data.lastPage == true) {
+    document.getElementById("monloader").setAttribute("style", "display:none");
+  }
 
-    if (data.dernierePage == true) {
-        document.getElementById("monloader").setAttribute("style", "display:none");
+  if (data.file_type == "waiter") {
+    if (divCree == false) {
+      // RH : We build the html page for preview the message.
+      let bodyA = document.body.innerHTML;
+      document.body.innerHTML = "";
+
+      let mainDiv = document.createElement("div");
+      mainDiv.setAttribute("style", "display: flex;");
+      document.body.appendChild(mainDiv);
+      mainDiv.id = "mainDiv";
+
+      let firstDiv = document.createElement("div");
+      mainDiv.appendChild(firstDiv);
+      firstDiv.setAttribute("style", "flex-grow: 1");
+      firstDiv.innerHTML = bodyA;
+
+      let secondDiv = document.createElement("div");
+      secondDiv.setAttribute("style", "width: 170px;padding-left:5px;");
+      mainDiv.appendChild(secondDiv);
+
+      firstDiv.id = "firstDiv";
+      secondDiv.id = "secondDiv";
+
+      let loader = new Image();
+      loader.id = "monloader";
+      loader.src = data.img;
+      secondDiv.appendChild(loader);
+      divCree = true;
     }
+  }
 
-    if (data.file_type == "waiter") {
+  // RH : This is the image who contains the page of the pdf document or just the image of the attachment.
+  if (data.type === "handle_me") {
+    let image = new Image();
+    image.src = data.image;
+    image.setAttribute(
+      "style",
+      "width:160;margin-bottom:10px;border:1px solid black;"
+    );
 
-        if (divCree == false) {
+    image.addEventListener("click", event => {
+      // Send a message when click on an page or image.
+      browser.runtime.sendMessage({
+        part_name: data.part_name,
+        numPage: data.numPage,
+        messageId: data.messageId,
+      }); 
+    });
 
-            // RH : We build the html page for preview the message.
-            var bodyA = document.body.innerHTML;
-            document.body.innerHTML = '';
+    // Add the image before the last element.
+    document
+      .getElementById("secondDiv")
+      .insertBefore(image, document.getElementById("monloader"));
 
-            var grandeDiv = document.createElement('div');
-            grandeDiv.setAttribute("style", 'display: flex;');
-            document.body.appendChild(grandeDiv);
-            grandeDiv.id = "GrandeDiv";
-
-            var DivPremiere = document.createElement('div');
-            grandeDiv.appendChild(DivPremiere);
-            DivPremiere.setAttribute("style", "flex-grow: 1");
-            DivPremiere.innerHTML = bodyA;
-
-            var DivSecond = document.createElement('div');
-            DivSecond.setAttribute("style", "width: 170px;padding-left:5px;");
-            grandeDiv.appendChild(DivSecond);
-
-
-            DivPremiere.id = "DivPremiere";
-            DivSecond.id = "second";
-
-
-
-            var loader = new Image();
-            loader.id = "monloader";
-            loader.src = data.img;
-            DivSecond.appendChild(loader);
-            divCree = true;
-        }
-    }
-
-    if (data.type === 'handle_me') {
-
-        var image = new Image();
-        image.src = data.image;
-        image.setAttribute("style", "width:160;margin-bottom:10px;border:1px solid black;"); // RH : This is the image who contains the page of the pdf document or just the image of the attachment.
-
-        image.addEventListener('click', event => {
-
-            browser.runtime.sendMessage({ "part_name": data.part_name, "numPage": data.numPage, "messageId": data.messageId }); //We send a message when we click on an page or image.
-        });
-
-        document.getElementById("second").insertBefore(image, document.getElementById("monloader")); //We put the image before the last element.
-
-        return Promise.resolve('done');
-    }
-    return false;
+    return Promise.resolve("done");
+  }
+  return false;
 });
 
-browser.runtime.sendMessage({ 'initialized': true });
+browser.runtime.sendMessage({ initialized: true });
--- pdfPreviewInMsg/popClick.js
+++ pdfPreviewInMsg/popClick.js
@@ -5,102 +5,118 @@ This extension for Thunderbird was made by : e-gaulue & rholeczy
 ----------------------------------------------------------------
 */
 
-
-async function pdfPreview(url, canvas, nbP) { // RH : We preview the pdf in the new window.
-    let pdf = await pdfjsLib.getDocument(url).promise;
-    let page = await pdf.getPage(nbP);
-    let viewport = await page.getViewport({ scale: 1.0 });
-    let scale = Math.min(canvas.width / viewport.width, canvas.height / viewport.height);
-    let thumbViewPort = await page.getViewport({ scale });
-    let context = canvas.getContext('2d');
-    page.render({ canvasContext: context, viewport: thumbViewPort });
+/**
+ * We preview the pdf in the new window.
+ * 
+ * @param {*} url 
+ * @param {*} canvas 
+ * @param {*} nbP 
+ */
+async function pdfPreview(url, canvas, nbP) {
+  let pdf = await pdfjsLib.getDocument(url).promise;
+  let page = await pdf.getPage(nbP);
+  let viewport = await page.getViewport({ scale: 1.0 });
+  let scale = Math.min(
+    canvas.width / viewport.width,
+    canvas.height / viewport.height
+  );
+  let thumbViewPort = await page.getViewport({ scale });
+  let context = canvas.getContext("2d");
+  page.render({ canvasContext: context, viewport: thumbViewPort });
 }
 
-
+/**
+ * <Description>
+ */
 async function generateThumbs() {
-    let url = new URL(window.location.href); // RH : We get the URL of the document.
-    var messageId = await parseInt(url.searchParams.get("messageId")); // rh :We get the messageID in the parameters of the URL.
-    var filename = await url.searchParams.get("filename"); // RH : We get the partName in the parameters of the URL.
-    var numPage = await parseInt(url.searchParams.get("numPage")); // RH : We get the number of the pages in the parameters of the URL.
-    let attachments = await messenger.messages.listAttachments(messageId);
-    for (let attachment of attachments) {
-        if (attachment.partName == filename) { // RH : If the partname get == the partname of the for
-            let file = await messenger.messages.getAttachmentFile(
-                messageId,
-                attachment.partName
-            );
-            let reader = new FileReader();
-            attachment.url = await new Promise((resolve) => {
-                reader.onload = (e) => {
-                    resolve(e.target.result);
-                };
-                reader.readAsDataURL(file);
-            });
+  let url = new URL(window.location.href); // RH : We get the URL of the document.
+  let messageId = await parseInt(url.searchParams.get("messageId")); // rh :We get the messageID in the parameters of the URL.
+  let filename = await url.searchParams.get("filename"); // RH : We get the partName in the parameters of the URL.
+  let numPage = await parseInt(url.searchParams.get("numPage")); // RH : We get the number of the pages in the parameters of the URL.
+  let attachments = await messenger.messages.listAttachments(messageId);
+  for (let attachment of attachments) {
+    if (attachment.partName == filename) {
+      // RH : If the partname get == the partname of the for
+      let file = await messenger.messages.getAttachmentFile(
+        messageId,
+        attachment.partName
+      );
+      let reader = new FileReader();
+      attachment.url = await new Promise(resolve => {
+        reader.onload = e => {
+          resolve(e.target.result);
+        };
+        reader.readAsDataURL(file);
+      });
 
-            var id = `attachmentElement_${messageId}_${attachment.partName}`;
-            var t = document.querySelector('#attachmentTemplate');
-            t.content.querySelector("div").id = id;
-            t.content.querySelector("p").textContent = filename;
-            document.body.appendChild(document.importNode(t.content, true));
+      let id = `attachmentElement_${messageId}_${attachment.partName}`;
+      let t = document.querySelector("#attachmentTemplate");
+      t.content.querySelector("div").id = id;
+      t.content.querySelector("p").textContent = filename;
+      document.body.appendChild(document.importNode(t.content, true));
 
+      // Event listeners cannot be attached to documentFragments before being added
+      // to the DOM. Find the new element.
+      let attachmentElement = document.getElementById(id);
 
-            // Event listeners cannot be attached to documentFragments before being added
-            // to the DOM. Find the new element.
-            var attachmentElement = document.getElementById(id);
+      for (let button of attachmentElement.querySelectorAll("button")) {
+        button.setAttribute("data-message-id", messageId);
+        button.setAttribute("data-attachment-part-name", attachment.partName);
+        button.addEventListener("click", clickHandler);
+      }
 
+      if (attachment.contentType.toLowerCase().startsWith("image/")) {
+        attachmentElement.querySelector("img").src = attachment.url;
+      }
+      // RH : On met notre image attachment dans une image.
+      else {
+        attachmentElement.querySelector("img").style.display = "none";
+      }
 
-            for (let button of attachmentElement.querySelectorAll("button")) {
-                button.setAttribute("data-message-id", messageId);
-                button.setAttribute("data-attachment-part-name", attachment.partName);
-                button.addEventListener("click", clickHandler);
-            }
-
-            if (attachment.contentType.toLowerCase().startsWith("image/"))
-                attachmentElement.querySelector("img").src = attachment.url; // RH : On met notre image attachment dans une image.
-            else
-                attachmentElement.querySelector("img").style.display = "none";
-
-            if (attachment.contentType.toLowerCase() == "application/pdf") {
-                pdfPreview(attachment.url, attachmentElement.querySelector("canvas"), numPage); // RH : On appelle la fonction pdfPreview.
-            } else {
-                attachmentElement.querySelector("canvas").style.display = "none";
-            }
-        }
-
+      if (attachment.contentType.toLowerCase() == "application/pdf") {
+        pdfPreview(
+          attachment.url,
+          attachmentElement.querySelector("canvas"),
+          numPage
+        ); // RH : On appelle la fonction pdfPreview.
+      } else {
+        attachmentElement.querySelector("canvas").style.display = "none";
+      }
     }
+  }
 }
-
 
 async function clickHandler(e) {
-    const file = await browser.messages.getAttachmentFile(
-        parseInt(e.target.dataset.messageId, 10),
-        e.target.dataset.attachmentPartName
-    );
-    const objectURL = URL.createObjectURL(file);
+  const file = await browser.messages.getAttachmentFile(
+    parseInt(e.target.dataset.messageId, 10),
+    e.target.dataset.attachmentPartName
+  );
+  const objectURL = URL.createObjectURL(file);
 
-    switch (e.target.getAttribute("action")) {
-        case "open":
+  switch (e.target.getAttribute("action")) {
+    // Open the pdf ? 
+    case "open":
+      {
+        let id = await browser.downloads.download({
+          filename: `_temp_${file.name}`,
+          saveAs: false,
+          url: objectURL,
+        });
+        browser.runtime.sendMessage({ file_open: id });
+      }
+      break;
 
-            {
-                let id = await browser.downloads.download({
-                    filename: `_temp_${file.name}`,
-                    saveAs: false,
-                    url: objectURL,
-                });
-                browser.runtime.sendMessage({ "file_open": id });
-
-            }
-            break;
-
-        case "download": // RH : For download the page.
-            {
-                await browser.downloads.download({
-                    filename: file.name,
-                    saveAs: true,
-                    url: objectURL,
-                });
-            }
-            break;
-    }
+    // Download the pdf ? 
+    case "download":
+      {
+        await browser.downloads.download({
+          filename: file.name,
+          saveAs: true,
+          url: objectURL,
+        });
+      }
+      break;
+  }
 }
+
 generateThumbs();
