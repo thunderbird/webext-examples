1a4f40d3eb749f06a41731ad1177177cbc319713
John Bieling <john.bieling@gmx.de>
2023-01-11 11:30:55 +0100

    add apiList example for MV3

---
+++ manifest_v3/apiList/apis.js
@@ -0,0 +1,36 @@
+let body = document.querySelector("body");
+let mainList = document.createElement("ul");
+body.appendChild(mainList);
+
+displayObject(browser, mainList);
+
+mainList.onclick = function({target}) {
+  target = target.closest("li");
+  if (target) {
+    target.classList.toggle("collapsed");
+  }
+};
+
+function displayObject(object, list) {
+  for (let key of Object.keys(object).sort()) {
+    let value;
+    try {
+      value = object[key];
+    } catch (ex) {
+      console.error(ex);
+    }
+    if (!value || !["function", "object"].includes(typeof value)) {
+      continue;
+    }
+    let item = document.createElement("li");
+    item.textContent = key;
+
+    if (typeof value == "object") {
+      item.classList.add("collapsed");
+      let newList = document.createElement("ul");
+      item.appendChild(newList);
+      displayObject(value, newList);
+    }
+    list.appendChild(item);
+  }
+}
---
+++ manifest_v3/apiList/background.js
@@ -0,0 +1,16 @@
+browser.action.onClicked.addListener(async () => {
+  browser.tabs.create({ url: "apis.html" });
+});
+
+async function injectIntoContent(tab, info) {
+  await browser.tabs.insertCSS(tab.id, {
+    file: "apis.css"
+  });
+  await browser.tabs.executeScript(tab.id, {
+    file: "apis.js"
+  })
+}
+
+browser.composeAction.onClicked.addListener(injectIntoContent);
+browser.messageDisplayAction.onClicked.addListener(injectIntoContent);
+
