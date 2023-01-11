c8e2f6b7d9a0fd1137e03eaae9238fb5a4e03277
John Bieling <john.bieling@gmx.de>
2023-01-11 13:25:37 +0100

    add legacyPrefMigration example for MV3

---
+++ manifest_v3/legacyPrefMigration/background.js
@@ -0,0 +1,56 @@
+// This is the current "migration" version. You can increase it later
+// if you happen to need to do more pref (or maybe other migrations) only once
+// for a user.
+const kCurrentLegacyMigration = 1;
+
+// This is the list of defaults for the legacy preferences. Note, you only
+// need to handle the defaults here. Regardless of if there are preferences
+// to be migrated or not, these values will be saved if the preference doesn't
+// exist.
+const kPrefDefaults = {
+  bool_pref: false,
+  integer_pref: 23,
+  ascii_string_pref: "default",
+  unicode_string_pref: "",
+};
+
+async function migratePrefs() {
+  // You could use any sub-section that you want here, it doesn't have
+  // to be called "preferences".
+  const results = await browser.storage.local.get("preferences");
+
+  const currentMigration =
+    results.preferences && results.preferences.migratedLegacy
+      ? results.preferences.migratedLegacy
+      : 0;
+
+  if (currentMigration >= kCurrentLegacyMigration) {
+    return;
+  }
+
+  let prefs = results.preferences || {};
+
+  if (currentMigration < 1) {
+    for (const prefName of Object.getOwnPropertyNames(kPrefDefaults)) {
+      prefs[prefName] = await browser.migratePrefs.getPref(prefName);
+      if (prefs[prefName] === undefined) {
+        prefs[prefName] = kPrefDefaults[prefName];
+      }
+    }
+  }
+
+  prefs.migratedLegacy = kCurrentLegacyMigration;
+  await browser.storage.local.set({ preferences: prefs });
+}
+
+// This is just a debug wrapper so you can see it in action. You could call
+// `migratePrefs().catch(console.error);` directly.
+async function main() {
+  // This triggers the migration.
+  await migratePrefs();
+
+  const results = await browser.storage.local.get("preferences");
+  console.log({ results });
+}
+
+main().catch(console.error);
---
+++ manifest_v3/legacyPrefMigration/readme.md
@@ -0,0 +1,3 @@
+## Legacy Preference Migration
+
+This extension shows how to migrate preferences from the legacy pref system to the WebExtension local storage.
