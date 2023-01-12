91bb168797db334ea6bf87cd337b18c385e1f4d0
John Bieling <john.bieling@gmx.de>
2023-01-12 08:24:42 +0100

    Fix listener not being registered as persistent, by moving its registration before awaiting any function calls

--- manifest_v3/experiment.restart/background.js
+++ manifest_v3/experiment.restart/background.js
@@ -3,6 +3,11 @@
 // persistent listener and the background will wake up (restart) each time the
 // event is fired. 
 
+// Note: In this example the two listeners are added inside an async function, but
+//       BEFORE an await is encountered. A listener registered after a function
+//       call has been awaited will not be registered as persistent and will not
+//       cause the background to wake up.
+
 function addRestartMenuEntry(window) {
     // Skip in case it is not the window we want to manipulate.
     // https://webextension-api.thunderbird.net/en/latest/windows.html#windowtype  
@@ -36,14 +41,15 @@ async function main () {
     // Register a listener for the onClick/onCommand event for any added menu entry.
     messenger.LegacyMenu.onCommand.addListener(onCommand);
 
+    // Register a listener for all newly opened windows so they get manipulated as well.
+    messenger.windows.onCreated.addListener(addRestartMenuEntry);
+
     // Loop over all already open windows and add the restart menu entry.
     const windows = await messenger.windows.getAll();
     for (let window of windows) {
         addRestartMenuEntry(window);
     }
 
-    // Register a listener for all newly opened windows so they get manipulated as well.
-    messenger.windows.onCreated.addListener(addRestartMenuEntry);
 }
 
 //Execute the asynchronous init() function.
