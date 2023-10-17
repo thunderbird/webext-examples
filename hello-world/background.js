ab1ef4301245e51aa25dd2d64c90932036edb295
John Bieling <john.bieling@gmx.de>
2023-10-17 09:32:21 +0200

    Update background.js

--- hello-world/background.js
+++ hello-world/background.js
@@ -45,13 +45,13 @@ await messenger.menus.onClicked.addListener(async (info, tab) => {
     }
 });
 
-// Register the message display script.
+// Register the message display script for all newly opened message tabs.
 messenger.messageDisplayScripts.register({
     js: [{ file: "messageDisplay/message-content-script.js" }],
     css: [{ file: "messageDisplay/message-content-styles.css" }],
 });
 
-// Inject script and CSS in all already open tabs.
+// Inject script and CSS in all already open message tabs.
 let openTabs = await messenger.tabs.query();
 let messageTabs = openTabs.filter(
     tab => ["mail", "messageDisplay"].includes(tab.type)
