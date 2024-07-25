8df3dc81f253d92d74bcf6c3e7bbc6ff2d905b76
John Bieling <john.bieling@gmx.de>
2024-07-25 21:38:28 +0200

    add loading a system mpdule

--- manifest_v2/experiment/api/ExampleAPI/implementation.js
+++ manifest_v2/experiment/api/ExampleAPI/implementation.js
@@ -5,9 +5,76 @@
 // Using a closure to not leak anything but the API to the outside world.
 (function (exports) {
 
+  // A helper to manage custom resource:// urls.
+  var resourceUrl = {
+    register(customNamespace, extensionId, folder) {
+      const { ExtensionParent } = ChromeUtils.importESModule(
+        "resource://gre/modules/ExtensionParent.sys.mjs"
+      );
+      const { ExtensionUtils } = ChromeUtils.importESModule(
+        "resource://gre/modules/ExtensionUtils.sys.mjs"
+      );
+      const { ExtensionError } = ExtensionUtils;
+      const resProto = Cc[
+        "@mozilla.org/network/protocol;1?name=resource"
+      ].getService(Ci.nsISubstitutingProtocolHandler);
+
+      if (resProto.hasSubstitution(customNamespace)) {
+        throw new ExtensionError(`There is already a resource:// url for the namespace "${customNamespace}"`);
+      }
+
+      let extension = ExtensionParent.GlobalManager.getExtension(extensionId);
+      let uri = Services.io.newURI(
+        folder || ".",
+        null,
+        extension.rootURI
+      );
+      resProto.setSubstitutionWithFlags(
+        customNamespace,
+        uri,
+        resProto.ALLOW_CONTENT_ACCESS
+      );
+    },
+
+    unloadAllModules(customNamespace) {
+      for (let module of Cu.loadedModules) {
+        let [schema, , namespace] = module.split("/");
+        if (
+          schema == "resource:" && 
+          customNamespace.toLowerCase() == namespace.toLowerCase()
+        ) {
+          console.log("Unloading module", module);
+          Cu.unload(module);
+        }
+      }
+    },
+    
+    unregister(customNamespace) {
+      const resProto = Cc[
+        "@mozilla.org/network/protocol;1?name=resource"
+      ].getService(Ci.nsISubstitutingProtocolHandler);
+      console.log("Unloading namespace", customNamespace);
+      resProto.setSubstitution(customNamespace, null);
+    },
+  }
+
+  // Register a resource:// url which points to the module folder. The name
+  // should be unique to avoid conflicts with other add-ons.
+  resourceUrl.register(
+    "exampleAddon1234",
+    "experiment@mv2.sample.extensions.thunderbird.net",
+    "modules/"
+  );
+
   // A helpful class for listening to windows opening and closing.
   var { ExtensionSupport } = ChromeUtils.importESModule(
     "resource:///modules/ExtensionSupport.sys.mjs"
+  );
+
+  // Load our own TestModule. This should be used as a last resort and only if
+  // the code is too complex to be part of the implementation file directly.
+  var { TestModule } = ChromeUtils.importESModule(
+    "resource://exampleAddon1234/TestModule.sys.mjs"
   );
 
   /**
@@ -105,7 +172,7 @@
 
           // A function.
           sayHello: async function (name) {
-            Services.wm.getMostRecentWindow("mail:3pane").alert(name);
+            TestModule.alert(name);
           },
 
           // An event. Most of this is boilerplate you don't need to worry about, just copy it.
@@ -132,18 +199,28 @@
     }
 
     onShutdown(isAppShutdown) {
-      // This function is called if the extension is disabled or removed, or Thunderbird closes.
-      // We usually do not have to do any cleanup, if Thunderbird is shutting down entirely
+      // This function is called if the extension is disabled or removed, or
+      // Thunderbird closes. We usually do not have to do any cleanup, if
+      // Thunderbird is shutting down entirely.
       if (isAppShutdown) {
         return;
       }
+      
+      // Unload all our modules (we loaded only one).
+      resourceUrl.unloadAllModules("exampleAddon1234");
+
+      // Unregister our resource:// url.
+      resourceUrl.unregister("exampleAddon1234");
+
+      // Flush all caches.
+      Services.obs.notifyObservers(null, "startupcache-invalidate");
 
       console.log("Goodbye world!");
     }
   };
 
-  // Export the api by assigning in to the exports parameter of the anonymous closure
-  // function, which is the global this.
+  // Export the api by assigning in to the exports parameter of the anonymous
+  // closure function, which is the global this.
   exports.ExampleAPI = ExampleAPI;
 
 })(this)
--- manifest_v2/experiment/readme.md
+++ manifest_v2/experiment/readme.md
@@ -1,7 +1,11 @@
 ## Experiment API
 
 This extension shows how to write an Experiment API including a function and an
-event. It also shows how to listen to custom events.
+event. It also shows how to listen to custom events. 
+
+The Experiment also registers a custom `resource://` url to load a system module
+bundled with the extension.
+**Note:** moving code into system modules should be avoided.
 
 For more information, check out the [documentation](https://thunderbird-webextensions.readthedocs.io/en/latest/how-to/experiments.html).
 
