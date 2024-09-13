fe00ededa14fc200377985a2df4632059f5f86d3
John Bieling <john.bieling@gmx.de>
2024-09-13 20:46:00 +0200

    Modernize openSearchDialog Experiment Example (MV3)

--- manifest_v3/experiment.openSearchDialog/background.js
+++ manifest_v3/experiment.openSearchDialog/background.js
@@ -1,8 +1,9 @@
 // Thunderbird can terminate idle backgrounds in Manifest V3.
-// Any listener directly added during add-on startup will be registered as a
-// persistent listener and the background will wake up (restart) each time the
-// event is fired. 
+// Any listener directly added during add-on startup in the top level scope will
+// be registered as a persistent listener and the background will wake up (restart)
+// each time the event is fired. Listeners have to be registered before the first
+// occurrence of an await keyword.
 
 browser.action.onClicked.addListener(() => {
-  browser.searchDialog.open();
+  browser.SearchDialog.open();
 });
